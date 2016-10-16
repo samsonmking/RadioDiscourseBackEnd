2d02bfa3adb298c25c1c8c6967a9c45e40914172
midnightspecial-io <samson@samsonmking.com>
2016-10-15 22:27:32 -0400

    Added deluge password and debug toggle to config.

--- radiodiscourse/rd_config.py
+++ radiodiscourse/rd_config.py
@@ -2,6 +2,8 @@ whatusername = 'pinkyoshimi'
 whatpassword = 'tabasco4Life'
 oauthpath = '/home/dev/oauth.cred'
 async_mode = None
+delugepassword = "deluge"
+debug = True
 users = {
     'Sam' : 'pizza12Slice'
 }
