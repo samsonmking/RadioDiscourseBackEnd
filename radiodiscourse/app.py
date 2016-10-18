005b6a0f3f20ee24c1d1410e5fcd9f60c43623dc
midnightspecial-io <samson@samsonmking.com>
2016-10-17 23:25:32 -0400

    Implemented flask static file serving for production client files.

--- radiodiscourse/app.py
+++ radiodiscourse/app.py
@@ -1,3 +1,4 @@
+from flask import send_from_directory
 from flask_restful import Api
 from socket_io import socketio
 from torrent import Torrent, Torrents
@@ -11,6 +12,10 @@ api.add_resource(Torrent, '/rd/api/torrent/<string:id>', endpoint = 'torrent')
 api.add_resource(Torrents, '/rd/api/torrents', endpoint = 'torrents')
 api.add_resource(WhatCD, '/rd/api/whatcd', endpoint = 'whatcd')
 api.add_resource(Token, '/rd/api/auth', endpoint = 'token')
+
+@app.route('/<path:path>')
+def root(path):
+    return send_from_directory('../public', path)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', debug=debug)
