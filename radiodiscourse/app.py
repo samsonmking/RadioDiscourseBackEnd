3943425c718a7a84e3d7d341fee4caf3d70f6bdf
midnightspecial-io <samson@samsonmking.com>
2016-10-21 01:38:07 +0000

    Added additional static route to redirect / to /index.html

--- radiodiscourse/app.py
+++ radiodiscourse/app.py
@@ -5,7 +5,7 @@ from torrent import Torrent, Torrents
 from what_cd import WhatCD
 from token_auth import Token
 from radiodiscourse import app
-from rd_config import debug
+from rd_config import debug, static_url_path
 
 api = Api(app)
 api.add_resource(Torrent, '/rd/api/torrent/<string:id>', endpoint = 'torrent')
@@ -13,6 +13,7 @@ api.add_resource(Torrents, '/rd/api/torrents', endpoint = 'torrents')
 api.add_resource(WhatCD, '/rd/api/whatcd', endpoint = 'whatcd')
 api.add_resource(Token, '/rd/api/auth', endpoint = 'token')
 
+@app.route('/', defaults={'path':'index.html'})
 @app.route('/<path:path>')
 def root(path):
     return send_from_directory('../public', path)
